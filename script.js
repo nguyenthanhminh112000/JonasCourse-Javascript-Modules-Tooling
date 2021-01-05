0f944a4cab48c9e49e679340c9c6b11c0520da50
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-01-05 23:32:18 +0700

    Learn how to use NPM

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-console.log('Importing Module');
+// --------------------------Javascript Module
 
-// // // Named Import
-// // import { addToCart, cost as price,cart } from "./shoppingCart.js";
-// // cart.push('Tea');
-// // console.log(price);
-// // addToCart('Milk',5);
+// console.log('Importing Module');
 
-// // Named Import all in one
-// import * as ShoppingCart from './shoppingCart.js'
-// console.log(ShoppingCart.cart);
+// // // // Named Import
+// // // import { addToCart, cost as price,cart } from "./shoppingCart.js";
+// // // cart.push('Tea');
+// // // console.log(price);
+// // // addToCart('Milk',5);
+
+// // // Named Import all in one
+// // import * as ShoppingCart from './shoppingCart.js'
+// // console.log(ShoppingCart.cart);
 
 
-// Default Import
-import ShoppingCart from './shoppingCart.js';
-// ShoppingCart('Milk',5);
-// ShoppingCart('Milk',5);
-// ShoppingCart('Milk',5);
-console.log(ShoppingCart);
+// // Default Import
+// import ShoppingCart from './shoppingCart.js';
+// // ShoppingCart('Milk',5);
+// // ShoppingCart('Milk',5);
+// // ShoppingCart('Milk',5);
+// console.log(ShoppingCart);
+
+// --------------------------Javascript NPM
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+const object ={
+    user:{active:false}
+};
+const cloneObject=cloneDeep(object)
+object.user.active=true;
+console.log(cloneObject);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,19 +1,19 @@
-console.log(`Exporting Module`);
-const cart=[];
-const cost=10;
+// console.log(`Exporting Module`);
+// const cart=[];
+// const cost=10;
 
-// Named Export
-// const addToCart=(product,quantity)=>{
+// // Named Export
+// // const addToCart=(product,quantity)=>{
+// //     cart.push({product,quantity});
+// //     console.log(cart);
+// // }
+// // export {cart,cost,addToCart};
+
+
+// // Default Export
+// const fnc=(product,quantity)=>{
 //     cart.push({product,quantity});
 //     console.log(cart);
 // }
-// export {cart,cost,addToCart};
-
-
-// Default Export
-const fnc=(product,quantity)=>{
-    cart.push({product,quantity});
-    console.log(cart);
-}
-export default fnc;
+// export default fnc;
 
